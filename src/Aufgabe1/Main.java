4b29b81c0f1ef7ff7153b0f4c5ec4b28487e4ec5
Your Name <you@example.com>
2019-01-16 08:30:26 +0100

    Aufgabe2 Try & Catch Block

--- src/Aufgabe1/Main.java
+++ src/Aufgabe1/Main.java
@@ -1,7 +1,34 @@
 package Aufgabe1;
 
+import javax.swing.*;
+
 public class Main {
     public static void main(String[] args) {
+        //Aufgabe1:
 
+        //Die Methode zifferPrüfen wird ausgeführt.
+        zifferPrüfen();
+    }
+    public static void zifferPrüfen()
+    {
+        //eine Variable eingabe erstellt, die zum schluss der Methode in die Konsole übergeben wird.
+        int eingabe = 0;
+
+        //Try catch block erstellt.
+        //Im Try block wird die zu prüfende Funktion eingegeben, wie hir die zuweißung der eingabe.(Mit JOptionPane)
+
+        try {
+         eingabe = Integer.parseInt(JOptionPane.showInputDialog(null,"Geben Sie eine Ziffer ein!"));
+        }
+
+        //Mit dem Catch block wirde der Try Block auf die Exception was man haben will überprüft. (hier NumberFormatException)
+        //Falls die Eingabe des Users richtig ist wird die letzte Zeile ausgeführt /sout
+        //Falls die Eingabe des Users falsch ist, wird die Methode wiederholt.
+        catch (NumberFormatException  e)
+        {
+            System.err.println("Die eingegebene Zahl war keine Ziffer!");
+            zifferPrüfen();
+        }
+        System.out.println("Die vom Nutzter eingegebene Zahl lautet: " + eingabe);;
     }
 }
